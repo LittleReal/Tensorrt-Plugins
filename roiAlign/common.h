be1335a688cc3210724afa30b05733274dd2e292
wzhirui <wzhirui@infervision.com>
2020-04-07 15:52:13 +0800

    add common.h

---
+++ roiAlign/common.h
@@ -0,0 +1,17 @@
+
+// Helper function for serializing plugin
+template <typename T>
+void writeToBuffer(char*& buffer, const T& val)
+{
+    *reinterpret_cast<T*>(buffer) = val;
+    buffer += sizeof(T);
+}
+
+// Helper function for deserializing plugin
+template <typename T>
+T readFromBuffer(const char*& buffer)
+{
+    T val = *reinterpret_cast<const T*>(buffer);
+    buffer += sizeof(T);
+    return val;
+}
